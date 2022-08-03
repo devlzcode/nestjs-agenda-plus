b7cf9c7a66db2b7c5beef29f1b989d1a08dbe55a
Ly <developer.lenneth@gmail.com>
2022-08-02 17:06:17 -0700

    Small bugfix for the last small bugfix

--- lib/agenda.module.ts
+++ lib/agenda.module.ts
@@ -1,5 +1,5 @@
 import { Module, OnApplicationShutdown } from "@nestjs/common";
-import { DiscoveryModule } from "@nestjs/core";
+import { DiscoveryModule, ModuleRef } from "@nestjs/core";
 import Agenda from "agenda";
 import { AgendaMetadataAccessor } from "./agenda.metadata-accessor";
 import { AgendaExplorer } from "./agenda.explorer";
@@ -13,12 +13,13 @@ export class AgendaModule
   extends ConfigurableModuleClass
   implements OnApplicationShutdown
 {
-  constructor(private readonly agenda: Agenda) {
+  constructor(private readonly modRef: ModuleRef) {
     super();
   }
 
   async onApplicationShutdown() {
-    await this.agenda.stop();
-    await this.agenda.close();
+    const agenda = this.modRef.get(Agenda, { strict: false });
+    await agenda.stop();
+    await agenda.close();
   }
 }
